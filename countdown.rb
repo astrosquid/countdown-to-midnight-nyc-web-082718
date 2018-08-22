baf4bbaea7e9c1060b80175859392ae60f423d1d
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-22 15:16:45 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(sec)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(sec)
+  while sec > 0 do
+    puts "#{sec} SECOND(S)!"
+    sleep(1)
+    sec -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end
